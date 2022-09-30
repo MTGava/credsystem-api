1ab809d5a6bb0d8c33b6419c1f25a723c67c8e34
Matheus Gava <matheus.gava3@gmail.com>
2022-09-29 22:11:46 -0300

    Começando classe de testes

--- src/main/java/com/api/credsystem/manager/utils/Utils.java
+++ src/main/java/com/api/credsystem/manager/utils/Utils.java
@@ -3,15 +3,20 @@ package com.api.credsystem.manager.utils;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Getter
 @Setter
 public class Utils {
-    public static String formatDate(Date date, String pattern) {
-        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
-        return sdf.format(date);
+    public static Date formatDate(String date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            return sdf.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException("Não foi possível formatar a data para o padrão da aplicação: " + e);
+        }
     }
 
     public static String generateRandomNumber(int length) {
